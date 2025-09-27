d03596cbe635ee3c6d640e4f505ad8dbf3670f4e
Grega <grega.pavlin95@gmail.com>
2025-09-27 22:13:20 +0200

    feat: Implement LiveClock with custom DD.MM.YYYY HH:MM:SS format

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState, useEffect } from 'react'
 import './App.css'
+import LiveClock from './components/LiveClock.jsx'
 
 function App() {
 
   return (
     <>
       <p>App will be here</p>
+      <LiveClock />
     </>
-  )
+  );
 }
 
 export default App
---
+++ client/src/components/LiveClock.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react'
+
+function LiveClock() {
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        const timerId = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(timerId);
+        };
+    }, []);
+
+    const day = time.getDate().toString().padStart(2, '0');
+    const month = (time.getMonth()+1).toString().padStart(2, '0');
+    const year = time.getFullYear().toString();
+    const hour = time.getHours().toString().padStart(2, '0');
+    const minute = time.getMinutes().toString().padStart(2, '0');
+    const second = time.getSeconds().toString().padStart(2, '0');
+
+    return (
+        <>
+            <div>
+                {day}.{month}.{year} {hour}:{minute}:{second}
+            </div>
+        </>
+    );
+}
+
+export default LiveClock
