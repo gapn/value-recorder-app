e78dd4dc363a0557f617dfbfb85dfd4e80f1128f
gapn <82774730+gapn@users.noreply.github.com>
2025-10-06 21:02:54 +0200

    refactor(WIP): Lift state and handlers to App component 2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,14 +9,52 @@ import SessionList from './components/SessionList.jsx';
 function App() {
 
   const [value, setValue] = useState(25.0);
+  const [isRecording, setIsRecording] = useState(false);
+  const [editingSessionId, setEditingSessionId] = useState(null);
+
+  const increaseValue = () => {
+    setValue(prevValue => parseFloat((prevValue + 0.1).toFixed(1)));
+  };
+
+  const decreaseValue = () => {
+    setValue(prevValue => parseFloat((prevValue - 0.1).toFixed(1)));
+  };
+
+  const toggleRecording = () => {
+    setIsRecording(prevIsRecording => !prevIsRecording);
+  }
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      event
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+
+  }, [])
 
   return (
     <>
       <p>App will be here</p>
       <LiveClock />
-      <ValueControl value={value} setValue={setValue} />
-      <RecordControl value={value} />
-      <SessionList />
+      <ValueControl 
+        value={value} 
+        onIncrease={increaseValue}
+        onDecrease={decreaseValue} 
+      />
+      <RecordControl 
+        value={value}
+        isRecording={isRecording}
+        onToggleRecording={toggleRecording}
+      />
+      <SessionList 
+        editingSessionId={editingSessionId}
+        setEditingSessionId={setEditingSessionId}
+      />
     </>
   );
 }
